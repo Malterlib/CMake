4b2377cc03c8bde697f8f6985cf6fd02320a07e5
Erik Olofsson <erik@olofsson.info>
2019-03-29 08:30:07 +0100

    Add Malterlib integration

--- Source/cmake.cxx
+++ Source/cmake.cxx
@@ -126,6 +126,7 @@
 #  include "cmExtraEclipseCDT4Generator.h"
 #  include "cmExtraKateGenerator.h"
 #  include "cmExtraSublimeTextGenerator.h"
+#  include "cmExtraMalterlibGenerator.h"
 #endif
 
 // NOTE: the __linux__ macro is predefined on Android host too, but
@@ -1917,6 +1918,7 @@ void cmake::AddDefaultExtraGenerators()
   this->ExtraGenerators.push_back(cmExtraEclipseCDT4Generator::GetFactory());
   this->ExtraGenerators.push_back(cmExtraKateGenerator::GetFactory());
   this->ExtraGenerators.push_back(cmExtraSublimeTextGenerator::GetFactory());
+  this->ExtraGenerators.push_back(cmExtraMalterlibGenerator::GetFactory());
 #endif
 }
 
